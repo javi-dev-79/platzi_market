a74d60809d7c693ad389ee5590901e93ad044197
javi-dev-79 <javi.dev.79@gmail.com>
2022-10-18 15:23:49 +0100

    Added Mappers.

---
+++ src/main/generated/com/platzi/market/persistence/mapper/CategoryMapperImpl.java
@@ -0,0 +1,49 @@
+package com.platzi.market.persistence.mapper;
+
+import com.platzi.market.domain.Category;
+import com.platzi.market.persistence.entity.Categoria;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2022-10-18T12:18:33+0100",
+    comments = "version: 1.5.2.Final, compiler: javac, environment: Java 11.0.16.1 (Eclipse Adoptium)"
+)
+@Component
+public class CategoryMapperImpl implements CategoryMapper {
+
+    @Override
+    public Category toCategory(Categoria categoria) {
+        if ( categoria == null ) {
+            return null;
+        }
+
+        Category category = new Category();
+
+        if ( categoria.getIdCategoria() != null ) {
+            category.setCategoryId( categoria.getIdCategoria() );
+        }
+        category.setCategory( categoria.getDescripcion() );
+        if ( categoria.getEstado() != null ) {
+            category.setActive( categoria.getEstado() );
+        }
+
+        return category;
+    }
+
+    @Override
+    public Categoria toCategoria(Category category) {
+        if ( category == null ) {
+            return null;
+        }
+
+        Categoria categoria = new Categoria();
+
+        categoria.setIdCategoria( category.getCategoryId() );
+        categoria.setDescripcion( category.getCategory() );
+        categoria.setEstado( category.isActive() );
+
+        return categoria;
+    }
+}
